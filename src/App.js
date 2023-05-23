142ed1b1a888dd1728739391e76a7b9fc91d7a5b
“Tomas <tomasbilbao05@gmail.com>
2023-05-23 18:50:23 -0300

    primera parte

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Counter from './components/Counter/Counter';
+import { useState } from 'react';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
+  
+  {/* 
+    const [view, setView] =useState(`Manzanas`)
+  */}
+
   return (
     <div className="App">
 
         <Navbar/>
 
         {/*<Counter/>*/}
+        
+        {/*
 
-        <ItemListContainer greeting={"Bienvenidos a New Horizons."}/>
+        <div>  
+          <button onClick={() => setView(`Manzanas`)} className="Botones">Manzanas</button>
+          <button onClick={() => setView(`Naranjas`)} className="Botones">Naranjas</button>
+          <button onClick={() => setView(`Ananás`)} className="Botones">Ananás</button>
+        </div>
+        
+        {view === `Manzanas` && <Counter title="Manzanas" min={5} max={15}/>}
+        {view === `Naranjas` && <Counter title="Naranjas" min={10} max={20}/>}
+        {view === `Ananás` && <Counter title="Ananás" min={10} max={20}/>}
+        
+        */}
+
+        <ItemListContainer greeting={"Bienvenido a New Horizons."}/>
     
+      <img src='https://i.ibb.co/94TXT95/Inicio.png' />
+
     </div>
   );
 }
